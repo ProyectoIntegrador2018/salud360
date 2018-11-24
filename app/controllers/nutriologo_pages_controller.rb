67c3a9546e23ff2b86f9e477f0789ffa9f0b834a
electricdrago <martinense96@gmail.com>
2018-11-24 14:12:17 -0600

    Arregle algunos bugs, y se ve mamalon

--- app/controllers/nutriologo_pages_controller.rb
+++ app/controllers/nutriologo_pages_controller.rb
@@ -2,12 +2,20 @@ class NutriologoPagesController < ApplicationController
   def metas_paciente
     @paciente = params[:id]
     @metas = paciente_metas(@paciente)
+    @porcentaje = 0
+    if @metas.count >0
+      @porcentaje = (@metas.where(cumplida: true).count*1.0/(@metas.count*1.0)*100).to_i()
+    end
     @comentarios = paciente_tips(@paciente)
   end
 
   def metas_cita
     @cita = params[:id]
     @metas = cita_metas(@cita)
+    @porcentaje = 0
+    if @metas.count >0
+      @porcentaje = (@metas.where(cumplida: true).count*1.0/(@metas.count*1.0)*100).to_i()
+    end
 
   end
 
