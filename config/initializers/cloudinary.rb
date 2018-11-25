516568f086e6c3933b66e1c5bff340de05274990
achavez95 <abchavez95@gmail.com>
2018-11-25 12:16:01 -0600

    Almacenamiento en la nube

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'hkm7dxrkk'
+  config.api_key = '886337378893585'
+  config.api_secret = '4k0XU6xlFh4Hls5QWwZmNz6F77Q'
+  config.secure = true
+  config.cdn_subdomain = true
+end
