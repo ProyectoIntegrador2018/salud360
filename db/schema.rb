77d64731eedf708962c8f75e3dcc0e17f37d6474
jvann <j.d.ivan69@gmail.com>
2019-03-21 01:18:54 -0600

    Enhancement[split-documents-in-phases]: Phase att migration to documents

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190225033950) do
+ActiveRecord::Schema.define(version: 20190318135917) do
 
   create_table "dieta", force: :cascade do |t|
     t.string "dietaPDF"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190225033950) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "description"
+    t.integer "phase", default: 1
   end
 
   create_table "horarios", force: :cascade do |t|
