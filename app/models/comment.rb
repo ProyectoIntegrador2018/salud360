cd6e606c8978f4848517afd796098874d38ef5c5
Victor Elizalde <vic-bull@hotmail.com>
2019-04-24 19:03:54 -0500

    Adds more validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :paciente, optional: true, :class_name => 'Paciente'
   belongs_to :nutriologo, optional: true, :class_name => 'Nutriologo'
+
+  validates :title, presence: true
+  validates :description, presence: true
 end
