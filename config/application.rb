2762fd8ce21d8041ce7816d9df86373ef276c87b
jvann <j.d.ivan69@gmail.com>
2019-05-01 03:44:51 -0500

    Refactor[layouts]: Create layouts for reuse to home_page and pacientes_page

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Salud360
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.to_prepare do
+      Devise::SessionsController.layout "home_page"
+  end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
